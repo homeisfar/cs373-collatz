01fa41091ef1e7e03d05bd338b50ee6f4c9b9d29
Ali Homafar <home.isfar@gmail.com>
2015-02-01 15:57:20 -0600

    Fixed cycle length count not updating per loop

--- Collatz.py
+++ Collatz.py
@@ -29,7 +29,6 @@ def collatz_eval (i, j) :
     j the end       of the range, inclusive
     return the max cycle length of the range [i, j]
     """
-    c = 1
     retval = c
     while i < j :
     	v = i
